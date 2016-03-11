7c345e35187d2fbddc0305784c9354f9bbf51a5f
hyp <yipinghe87@163.com>
2016-03-11 11:30:06 +0800

    Demo2: Dependency Management. Run Command: gradle test
    http://blog.jobbole.com/72992/

--- gradle/src/main/java/com/he/demo/HelloWorld.java
+++ gradle/src/main/java/com/he/demo/HelloWorld.java
@@ -1,8 +1,15 @@
 package com.he.demo;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+
+	private static final Logger LOGGER = Logger.getLogger(HelloWorld.class);
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		MessageService messageService = new MessageService();
+		
+		String message = messageService.getMessage();
+		LOGGER.info("Received message: " + message);
 	}
 }
---
+++ gradle/src/main/java/com/he/demo/MessageService.java
@@ -0,0 +1,7 @@
+package com.he.demo;
+
+public class MessageService {
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
